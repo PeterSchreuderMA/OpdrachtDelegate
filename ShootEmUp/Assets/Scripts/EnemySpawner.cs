f5d7d7f801ba7d059f2b87ac8a1bbc2fcd533b3b
PeterSchreuder_Laptop <24441@ma-web.nl>
2019-01-11 10:06:01 +0100

    Added - Script Folder

---
+++ ShootEmUp/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour 
+{
+
+	[SerielizedField]
+	private List<GameObject>();
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
